16f29bb3f781ece653947cdc3b5a014facf7e6ff
Fady Emad Shoukry <121491487+fadyemad98@users.noreply.github.com>
2024-09-19 00:04:26 +0300

    Slave

---
+++ Final_Project_Slave/Debug/MCAL/SPI_DRIVER/STM32_F103C6_SPI_Config.d
@@ -0,0 +1,17 @@
+MCAL/SPI_DRIVER/STM32_F103C6_SPI_Config.o: \
+ ../MCAL/SPI_DRIVER/STM32_F103C6_SPI_Config.c \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_SPI_Driver.h \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32F103x6.h \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_GPIO_Driver.h \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_RCC_Driver.h \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_GPIO_Driver.h
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_SPI_Driver.h:
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32F103x6.h:
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_GPIO_Driver.h:
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_RCC_Driver.h:
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_GPIO_Driver.h:
---
+++ Final_Project_Slave/Debug/MCAL/SYSTICK_DRIVER/STM32_F103C6_SYSTICK_Config.d
@@ -0,0 +1,14 @@
+MCAL/SYSTICK_DRIVER/STM32_F103C6_SYSTICK_Config.o: \
+ ../MCAL/SYSTICK_DRIVER/STM32_F103C6_SYSTICK_Config.c \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_SYSTICK_DRIVER.h \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32F103x6.h \
+ C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/OS/KERNEL_interface.h \
+ ../Inc/STD_TYPES.h
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32_F103C6_SYSTICK_DRIVER.h:
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/MCAL/Inc/STM32F103x6.h:
+
+C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Slave/OS/KERNEL_interface.h:
+
+../Inc/STD_TYPES.h:
