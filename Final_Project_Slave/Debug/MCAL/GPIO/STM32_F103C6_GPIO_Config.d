bd74bc39934d17849d93ba4d82bc1f070583c7d0
Fady Emad Shoukry <121491487+fadyemad98@users.noreply.github.com>
2024-09-21 19:22:43 +0300

    Delete Final_Project_Slave directory

--- Final_Project_Slave/Debug/MCAL/GPIO/STM32_F103C6_GPIO_Config.d
+++
@@ -1,8 +0,0 @@
-MCAL/GPIO/STM32_F103C6_GPIO_Config.o: \
- ../MCAL/GPIO/STM32_F103C6_GPIO_Config.c \
- C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Master/MCAL/Inc/STM32_F103C6_GPIO_Driver.h \
- C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Master/MCAL/Inc/STM32F103x6.h
-
-C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Master/MCAL/Inc/STM32_F103C6_GPIO_Driver.h:
-
-C:/Users/fadye/STM32CubeIDE/workspace_1.4.0/Final_Project_Master/MCAL/Inc/STM32F103x6.h:
